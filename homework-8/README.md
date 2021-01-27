3aab8d46842ba407a60fcda93b8194835bc98410
[Stanislav Zavialov] <xslvnsh8x@yandex.ru>
2021-01-27 21:44:28 +0300

    homework-8 todo-app

---
+++ homework-8/README.md
@@ -0,0 +1,17 @@
+## Project setup
+```
+npm i
+```
+
+
+### Compiles for production
+```
+npm run build
+
+```
+
+
+### Compiles for development
+```
+npm run dev
+```
---
+++ homework-8/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  // entry: './app/index.js',
+  entry: {
+    main: ['@babel/polyfill', './app/index.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './static/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  }
+};
